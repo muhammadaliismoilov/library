fc81bb899e7b217bdb9324a3a17bcbce8722d142
muhammadaliismoilov <rustammuborakmuhammadali@gmail.com>
2025-05-06 12:54:46 +0500

    swagger

--- Utils/swagger.js
+++ Utils/swagger.js
@@ -1,14 +1,42 @@
 const swaggerJsdoc = require("swagger-jsdoc");
+// const options = {
+//   definition: {
+//     openapi: `3.0.0`,
+//     info: {
+//       title: `Kutubxona loyhasi`,
+//       version: `1.0.0`,
+//       description: `Bu kutubxona loyhasining dokumentetsiyasi`,
+//     },
+
+//   },
+//   apis: [`./routes/*.js`],
+// };
+
 const options = {
   definition: {
-    openapi: `3.0.0`,
+    openapi: '3.0.0',
     info: {
-      title: `Kutubxona loyhasi`,
-      version: `1.0.0`,
-      description: `Bu kutubxona loyhasining dokumentetsiyasi`,
+      title: 'Car Management API',
+      description: 'Avtomobillarni boshqarish uchun API: autentifikatsiya, kategoriyalar, avtomobillar, profil va admin funksiyalari.',
+      version: '1.0.0',
+    },
+    servers: [
+      {
+        url:" http://localhost:${process.env.PORT || 4001}",
+        description: 'Mahalliy server',
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
     },
   },
-  apis: [`./routes/*.js`],
+  apis: ['./routes/*.routes.js'], // Barcha marshrut fayllaridagi Swagger kommentariyalarini o‘qish
 };
 const swaggerDocument = swaggerJsdoc(options);
 module.exports = swaggerDocument;
